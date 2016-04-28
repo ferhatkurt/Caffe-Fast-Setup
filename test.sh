18a3461fe3de5cc3a91bce0b31310e2437562603
Ferhat Kurt <openzeka@gmail.com>
2016-04-28 15:10:47 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+echo "
+server {
+    location / {
+        proxy_pass http://localhost:5000;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+    }
+    location /statica {
+        alias  /home/www/flask_project/static/;
+    }
+}" >> /etc/nginx/sites-available/web_demo
