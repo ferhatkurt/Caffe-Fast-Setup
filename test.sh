1d8c27b3a5ff1fe2e12a83e8d241b6af759841fb
Ferhat Kurt <openzeka@gmail.com>
2016-04-28 15:14:15 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,12 @@
 
-cd /etc/nginx/sites-available
-echo "
+sudo echo "
 server {
     location / {
         proxy_pass http://localhost:5000;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
     }
-    location /statica {
+    location /statical {
         alias  /home/www/flask_project/static/;
     }
 }" >> web_demo
