5c86a37a000697cfcb4d571dbe2d2efd778e836d
Ferhat Kurt <openzeka@gmail.com>
2016-04-28 15:27:28 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
-cd /etc/nginx/sites-available
-echo "
-server {
-    location / {
-        proxy_pass http://localhost:5000;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-    }
-    location /elma {
-        alias  /home/www/flask_project/static/;
-    }
-}" >> web_demo
+# cd /etc/nginx/sites-available
+# echo "
+# server {
+#     location / {
+#         proxy_pass http://localhost:5000;
+#         proxy_set_header Host $host;
+#         proxy_set_header X-Real-IP $remote_addr;
+#     }
+#     location /elma {
+#         alias  /home/www/flask_project/static/;
+#     }
+# }" >> web_demo
+
+echo 'text' | sudo tee -a /etc/nginx/sites-available
